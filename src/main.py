33133a1bf2a974c04129d884c88d88c080576186
Felix Hoffmann <felix.emmanuel.hoffmann@gmail.com>
2022-12-09 13:49:33 +0100

    🚧 Improving threshold using `hsv`

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+import os
 import sys
+from dataclasses import dataclass
 from glob import glob
 
 import cv2 as cv
@@ -17,7 +19,16 @@ from src.utils.shell import clear_shell, get_int, print_options
 # from utils.calibration import get_calibration_params
 
 
-def pipeline(img: cv.Mat, mtx: cv.Mat, dist: cv.Mat) -> cv.Mat:
+@dataclass
+class ConvertedImage:
+    undist: cv.Mat | None = None
+    thresh: cv.Mat | None = None
+    region: cv.Mat | None = None
+    transform: cv.Mat | None = None
+    poly: cv.Mat | None = None
+
+
+def pipeline(img: cv.Mat, mtx: cv.Mat, dist: cv.Mat) -> ConvertedImage:
     # Undistortion("calibration", mtx, dist),
     # Segmentation("segmentation"),
     # PerspectiveTransformation("perspective_transformation"),
@@ -27,11 +38,11 @@ def pipeline(img: cv.Mat, mtx: cv.Mat, dist: cv.Mat) -> cv.Mat:
     undist = undist_img(img, mtx, dist)
     # TODO: Think about blurring image before thresholding
     thresh = thresh_img(undist)
-    region = segment_img(thresh)
-    transform = transform_perspective(region)
-    poly = fit_polynomial(transform)
+    # region = segment_img(thresh)
+    # transform = transform_perspective(region)
+    # poly = fit_polynomial(transform)
 
-    return poly
+    return ConvertedImage(undist, thresh)  # , region, transform, poly
 
 
 def main() -> None:
@@ -52,7 +63,7 @@ def main() -> None:
     )
     dist = cv.Mat(np.array([[-0.24688775, -0.02373133, -0.00109842, 0.00035108, -0.00258571]]))
 
-    options_1: list[str] = ["images", "calib", "videos"]
+    options_1: list[str] = os.listdir("./data/exam")
     print_options(options_1)
     user_input_1 = get_int(options_1)
 
@@ -63,7 +74,7 @@ def main() -> None:
     glob_files = glob(f"./data/exam/{options_1[user_input_1 - 1]}/*")
 
     # The user wants to run the pipeline on images
-    if user_input_1 == 1 or user_input_1 == 2:
+    if options_1[user_input_1 - 1] == "images" or options_1[user_input_1 - 1] == "calib":
         logger.debug("Running pipeline on images...")
         print_options(glob_files)
         user_input_2 = get_int(glob_files)
@@ -74,13 +85,18 @@ def main() -> None:
 
         # Load the image and run the pipeline
         img = cv.cvtColor(cv.imread(glob_files[user_input_2 - 1]), cv.COLOR_BGR2RGB)
-        processed_img = pipeline(img, mtx, dist)
+        converted_image = pipeline(img, mtx, dist)
 
-        plt.imshow(processed_img)
+        img_to_plot = converted_image.thresh
+        if img_to_plot is None:
+            logger.error("The image could not be converted. Exiting...")
+            sys.exit(1)
+
+        plt.imshow(img_to_plot)
         plt.show()
 
     # The user wants to run the pipeline on videos
-    if user_input_1 == 3:
+    if options_1[user_input_1 - 1] == "videos":
         logger.debug("Running pipeline on videos...")
         print_options(glob_files)
         user_input_2 = get_int(glob_files)
@@ -100,10 +116,15 @@ def main() -> None:
                 break
 
             # Apply pipeline
-            processed_frame = cv.cvtColor(pipeline(frame, mtx, dist), cv.COLOR_RGB2BGR)
+            converted_frame = pipeline(frame, mtx, dist)
+
+            frame_to_plot = converted_frame.thresh
+            if frame_to_plot is None:
+                logger.error("The image could not be converted. Exiting...")
+                sys.exit(1)
 
             # render new frame
-            cv.imshow("frame", processed_frame)
+            cv.imshow("frame", frame_to_plot)
             if cv.waitKey(1) == ord("q"):
                 break
 
--- src/pipeline/thresholding.py
+++ src/pipeline/thresholding.py
@@ -6,52 +6,49 @@ import numpy as np
 # TODO: Fix operations (typing)
 # TODO: Add docstring
 def thresh_img(img: cv.Mat) -> cv.Mat:
+    img_gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+    img_hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+    # TODO: Check hsv values
     colors = {
-        "yellow": {"min": np.array([150, 150, 0]), "max": np.array([255, 255, 120])},
-        "white": {"min": np.array([185, 185, 185]), "max": np.array([255, 255, 255])},
+        "yellow": {"min": np.array([0, 15, 215]), "max": np.array([179, 50, 255])},
+        "white": {"min": np.array([15, 50, 225]), "max": np.array([35, 225, 255])},
     }
-    saturation_threshold = (180, 200)
-    sobel_x_threshold = (40, 100)
-    hsv_s_threshold = (200, 215)
-
-    # Isolate the blue channel for sobel filtering, so the yellow line is easily detected
-    color_channel = img[:, :, 2]
-
-    # Convert to HLS color space and isolate the saturation channel
-    hls_channel = cv.cvtColor(img, cv.COLOR_RGB2HLS)[:, :, 2]
-
-    # Convert to HVS color space and isolate the light channel
-    hsv_channel = cv.cvtColor(img, cv.COLOR_RGB2HSV)[:, :, 1]
-
-    yellow_mask = cv.inRange(img, colors["yellow"]["min"], colors["yellow"]["max"])
-    white_mask = cv.inRange(img, colors["white"]["min"], colors["white"]["max"])
+    yellow_mask = cv.inRange(img_hsv, colors["yellow"]["min"], colors["yellow"]["max"])
+    white_mask = cv.inRange(img_hsv, colors["white"]["min"], colors["white"]["max"])
+    color_mask = cv.bitwise_or(yellow_mask, white_mask)
+    color_mask[color_mask > 0] = 1
 
     # Apply Sobel x filtering on the blue color channel
+    color_channel = img[:, :, 2]  # Isolate the blue channel for sobel filtering, so the yellow line is easily detected
     sobelx = cv.Sobel(color_channel, cv.CV_64F, 1, 0)
     abs_sobelx = np.absolute(sobelx)
     scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
 
     # Apply the threshold on the x gradient
+    sobel_x_threshold = np.array([40, 100])
     sxbinary = np.zeros_like(scaled_sobel)
     sxbinary[(scaled_sobel >= sobel_x_threshold[0]) & (scaled_sobel <= sobel_x_threshold[1])] = 1
+    sobel_mask = cv.bitwise_or(sxbinary, color_mask)
 
     # Apply the threshold on the hls saturation channel
+    saturation_threshold = np.array([180, 200])
+    hls_channel = cv.cvtColor(img, cv.COLOR_RGB2HLS)[
+        :, :, 2
+    ]  # Convert to HLS color space and isolate the saturation channel
     hls_s_binary = np.zeros_like(hls_channel)
     hls_s_binary[(hls_channel >= saturation_threshold[0]) & (hls_channel <= saturation_threshold[1])] = 1
+    hls_mask = cv.bitwise_or(hls_s_binary, sobel_mask)
 
     # Apply the threshold on the hsv saturation channel
+    hsv_s_threshold = np.array([200, 215])
+    hsv_channel = cv.cvtColor(img, cv.COLOR_RGB2HSV)[
+        :, :, 1
+    ]  # Convert to HVS color space and isolate the light channel
     hsv_s_binary = np.zeros_like(hsv_channel)
     hsv_s_binary[(hsv_channel >= hsv_s_threshold[0]) & (hsv_channel <= hsv_s_threshold[1])] = 1
+    hsv_mask = cv.bitwise_or(hsv_s_binary, hls_mask)
 
-    # Apply the threshold on the yellow mask
-    yellow_binary = (yellow_mask // 255).astype(np.uint8)
+    filtered_image = cv.bitwise_and(img_gray, img_gray, mask=hsv_mask)
 
-    # Apply the threshold on the white mask
-    white_binary = (white_mask // 255).astype(np.uint8)
-
-    combined_binary = np.zeros_like(sxbinary)
-    combined_binary[
-        (hls_s_binary == 1) | (sxbinary == 1) | (hsv_s_binary == 1) | (yellow_binary == 1) | (white_binary == 1)
-    ] = 1
-
-    return combined_binary
+    return filtered_image
